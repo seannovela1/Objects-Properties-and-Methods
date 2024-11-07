063d9170ee090615cb61d5303b553ad39428345b
seannovela1 <seannovela5484@my.whccd.edu>
2024-11-06 19:14:04 -0800

    added script.js and myArray

---
+++ script.js
@@ -0,0 +1,6 @@
+// Step 1: Instantiate a new array and add 5 numeric values
+let myArray = [10, 20, 30, 40, 50];
+
+// Step 2: Output the first and last value of the array
+console.log("first value:",myArray[0]); // First value: 10
+console.log("Last value:", myArray[myArray.length - 1]); // Last value: 50
